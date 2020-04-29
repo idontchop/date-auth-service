2b9b5b30bf520127cdb8dbc6c8a7fbeba2482479
Nate D <nate@idontchop.com>
2020-04-29 09:32:41 -0700

    Initial Commit

---
+++ src/main/java/com/idontchop/dateauthservice/DateAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.idontchop.dateauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateAuthServiceApplication.class, args);
+	}
+
+}
